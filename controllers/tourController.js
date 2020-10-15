5dd94fef3ff5a55d554a8e6f1d14e007d9a76b65
Juan Ro <juanro.pf@gmail.com>
2020-10-15 15:51:51 -0500

    Until class 110

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -10,7 +10,11 @@ const aliasTopTours= (req, res, next) => {
 
 const getAllTours= async (req, res) => {
   try {
-    const features= new APIFeatures(Tour.find(), req.query).filter().sort().limitFields().paginate();
+    const features= new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
     const tours= await features.query;
 
     res.status(200).json({
