7bc4520564af70abbb8eb3c67ed0a214f32e85f7
Juan Ro <juanro.pf@gmail.com>
2020-11-08 19:43:14 -0600

    Until class 187

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path= require('path');
 const express= require('express');
 const morgan= require('morgan');
 const rateLimit= require('express-rate-limit');
@@ -11,10 +12,18 @@ const globalErrorHandler= require('./controllers/errorController');
 const tourRouter= require('./routes/tourRoutes');
 const userRouter= require('./routes/userRoutes');
 const reviewRouter= require('./routes/reviewRoutes');
+const viewRouter= require('./routes/viewRoutes');
 
 const app= express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // Global Middlewares
+// Serving static files
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Set secyrity HTTP headers
 app.use(helmet());
 
@@ -47,9 +56,6 @@ app.use(hpp({
   whitelist: ['duration', 'ratingsQuantity', 'ratingsAverage', 'maxGroupSize', 'difficulty', 'price']
 }));
 
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
-
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime= new Date().toISOString();
@@ -57,7 +63,7 @@ app.use((req, res, next) => {
 });
 
 // Routes
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
---
+++ routes/viewRoutes.js
@@ -0,0 +1,12 @@
+const express= require('express');
+const { getOverview, getTour, getLoginForm } = require('../controllers/viewController');
+
+const router= express.Router();
+
+router.get('/', getOverview);
+
+router.get('/tour/:slug', getTour);
+
+router.get('/login', getLoginForm);
+
+module.exports= router;
