ee111e31ecd19ba1c838db0252501592bdfe317c
Juan Ro <juanro.pf@gmail.com>
2020-11-13 08:49:12 -0600

    CORS implemented

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss= require('xss-clean');
 const hpp= require('hpp');
 const cookieParser= require('cookie-parser');
 const compression= require('compression');
+const cors= require('cors');
 
 const AppError= require('./utils/appError');
 const globalErrorHandler= require('./controllers/errorController');
@@ -25,6 +26,15 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // Global Middlewares
+// Implement CORS
+app.use(cors()); // TO allow all origins
+
+// app.use(cors({ // In case front end is in a different origin than backend
+//   origin: 'https://www.frontend.com'
+// }));
+
+app.options('*', cors()); // For OPTIONS method
+
 // Serving static files
 // app.use(express.static(`${__dirname}/public`));
 app.use(express.static(path.join(__dirname, 'public')));
